f61e2f1d5fafaec9b44389948f1f03e12b731ccb
Ibrahim Sakaci <ibrahim@unico.aero>
2019-11-30 22:50:07 +0300

    h1 duzenlendi

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1 class="text-center">The Super Quiz</h1>
+        <h1 class="text-center">The Super Math Quiz</h1>
         <p class="text-center">by Jokerinya</p>
       </div>
     </div>
