3e3771e6293c72bb50b81f6c25a08a031372e821
Ibrahim Sakaci <ibrahim@unico.aero>
2019-11-30 22:44:49 +0300

    imza eklendi

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1 class="text-center">The Super Quiz</h1>
+        <p class="text-center">by Jokerinya</p>
       </div>
     </div>
     <hr />
